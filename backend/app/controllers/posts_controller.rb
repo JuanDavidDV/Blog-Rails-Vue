d09e11fd4b2be7a8a1419ae540d4ac86d10c972e
JuanDavidDV <juandadazav@gmail.com>
2025-05-24 22:38:10 -0400

    add private param and create post vue filer

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :body ])
+      params.expect(post: [ :id, :title, :body ])
     end
 end
