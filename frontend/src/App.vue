92d1e84b7f80698cb1376bcfb22d93f7fa306c9f
JuanDavidDV <juandadazav@gmail.com>
2025-05-24 22:43:01 -0400

    update CORS and rename file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
+<script setup>
+import Posts from './components/Posts.vue'
+</script>
+
 <template>
   <div>
     <Posts />
   </div>
 </template>
-
-<script setup>
-import Posts from './components/Posts.vue'
-</script>
 
 <style scoped>
 </style>
