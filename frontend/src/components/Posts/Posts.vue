146ea594469b612f1090eac3d4dbe7be87bcac0c
JuanDavidDV <juandadazav@gmail.com>
2025-05-25 12:29:45 -0400

    create functions

--- frontend/src/components/Posts/Posts.vue
+++ frontend/src/components/Posts/Posts.vue
@@ -12,8 +12,39 @@
           class="body-input"
     />
   </div>
+  <!-- only renders if editing the post-->
+  <button v-if="isEditing" @click="updatePost">Update</button>
+  <button v-if="isEditing" @click="cancelEdit">Cancel</button>
+
+  <!--only renders if not editing the post-->
+  <button v-else @click="createPost">Create</button>
 </template>
 
 <script setup>
   import "./Posts.css"
+  import { ref, onMounted } from "vue";
+
+  const posts = ref([]);
+  const title = ref("");
+  const body = ref("");
+  const postId = ref(0);
+  const isEditing = ref(false);
+
+  const API_URL = "http://localhost:3000/post";
+
+  const createPost = async() => {
+    const res = await fetch(API_URL, {
+      method: "POST",
+
+    })
+  };
+
+
+  const updatePost = async() => {
+    return true;
+  };
+
+  const cancelEdit = () => {
+    return true;
+  };
 </script>
